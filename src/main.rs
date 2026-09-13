e7e8e31711035e4ad90b94e1d9866391cf4eeb1b
Isaac Anderson <isaac@dw-perspective.org.uk>
2026-09-13 00:10:37 -0400

    progess(api call to dictionaryapi.dev)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use reqwest::Client;
+
 fn main() {
-    println!("Hello, world!");
+    // Get the word
+    print!("What's the word?");
+    let mut word = "".to_string();
+    std::io::stdin()
+        .read_line(&mut word)
+        .expect("Failed to readline");
+    let word = word.trim();
+
+    // Check the word
+    if word.is_empty() || !word.chars().all(char::is_alphabetic) {
+        println!("Word is empty or contains unexpected chars");
+    }
+
+    // Search the wordA
+    let client = Client::new();
+    let response = client
+        .get("https://api.dictionaryapi.dev/api/v2/entries/en/word")
+        .send()
+        .unwrap();
 }
